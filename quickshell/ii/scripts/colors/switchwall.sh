b18b9dd1173c1371932af5ee1d32fe3a3887daa8
Avishka D. Gamage <avishkakavinda@proton.me>
2025-08-19 15:40:36 +0530

    Update configuration files and replace terminal references
    Modified gtkrc and gtkrc-2.0 to reflect the new creation timestamps. Added DirSelectDialog size settings in kdeglobals and enabled the display of hidden files. Updated kdialogrc to set a maximized window size for file dialogs. Changed the terminal application from 'kitty' to 'alacritty' in env.conf and adjusted keybinds to reflect this change. Removed obsolete kitty configuration files and updated wallpaper paths in hyprlock and illogical-impulse configurations.

--- quickshell/ii/scripts/colors/switchwall.sh
+++ quickshell/ii/scripts/colors/switchwall.sh
@@ -99,7 +99,7 @@ check_and_prompt_upscale() {
                         "Install Upscayl?" \
                         "yay -S upscayl-bin")
                     if [[ "$action2" == "install_upscayl" ]]; then
-                        kitty -1 yay -S upscayl-bin
+                        alacritty -e yay -S upscayl-bin
                         if command -v upscayl &>/dev/null; then
                             nohup upscayl > /dev/null 2>&1 &
                         fi
@@ -210,7 +210,7 @@ switch() {
                     "Can't switch to video wallpaper" \
                     "Missing dependencies: ${missing_deps[*]}")
                 if [[ "$action" == "install_arch" ]]; then
-                    kitty -1 sudo pacman -S "${missing_deps[*]}"
+                    alacritty -e sudo pacman -S "${missing_deps[*]}"
                     if command -v mpvpaper &>/dev/null && command -v ffmpeg &>/dev/null; then
                         notify-send 'Wallpaper switcher' 'Alright, try again!' -a "Wallpaper switcher"
                     fi
