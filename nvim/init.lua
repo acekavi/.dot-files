c3f7ca6b7a1ed40273fbc7e10fb9e102ae5bca56
Avishka D. Gamage <avishkakavinda@proton.me>
2025-10-03 19:15:26 +0530

    Refactor color schemes and styles across multiple configuration files
    - Updated GTK CSS to enhance headerbar and window control styles, including color adjustments and box shadows for better aesthetics.
    - Modified color definitions in `colors.kdl` to improve focus and border colors for UI elements.
    - Changed active and inactive color schemes in `matugen.conf` for a more cohesive look.
    - Adjusted prompt colors in `starship.toml` to create a more visually appealing terminal experience.
    - Added Neovim configuration to integrate Wayland clipboard support for seamless copy-paste functionality.

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+vim.g.clipboard = {
+     name = 'WLClipboard',
+     copy = {
+       ['+'] = 'wl-copy',
+       ['*'] = 'wl-copy',
+     },
+     paste = {
+       ['+'] = 'wl-paste',
+       ['*'] = 'wl-paste',
+     },
+     cache_enabled = 0,
+   }
